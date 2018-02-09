66f82bdb44761f32b76bc3de1999c7644fb3d4e7
lkosmala <kosmala.luke@gmail.com>
2018-02-09 19:22:32 +0100

    test of different headers option to get authorization token

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,4 +1,4 @@
-import { Response, Headers } from '@angular/http';
+import { Response, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
@@ -6,23 +6,69 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
-
 @Injectable()
 export class AuthenticationService {
+    headers_new: Headers;
+    options_new: RequestOptions;
 
-    private authUrl = 'http://localhost:8080/auth';
-    private headers = new Headers({ 'Content-Type': 'application/json' });
+    private authUrl = 'http://localhost:8080/filmdb/login';
 
     constructor(private http: HttpClient) {
+        this.headers_new = new Headers({
+            'Content-Type': 'application/json',
+            'Accept': 'q=0.8;application/json;q=0.9',
+            'Access-Control-Expose-Headers': 'Authorization'
+        });
+        this.options_new = new RequestOptions({ headers: this.headers_new });
     }
 
     login(username: string, password: string): Observable<boolean> {
+        const headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Authorization', 'Bearer');
+        headers.append('Access-Control-Allow-Headers', 'Authorization');
+
+        const httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+            headers1: new HttpHeaders({ 'Access-Control-Expose-Headers': 'Authorization' }),
+            headers2: new HttpHeaders({ 'Access-Control-Allow-Headers': 'Authorization'}),
+            headers3: new HttpHeaders({ 'Authorization': 'Bearer' }),
+        };
+
+        const test = this.http.post(this.authUrl, { username: username, password: password }, {headers: headers});
+
+        console.log(test);
+
+        const object = this.http.post(this.authUrl, JSON.stringify({ username: username, password: password }), 
+        { headers: headers, observe: 'response' })
+            .subscribe(
+            (resp) => {
+                let header: HttpHeaders = resp.headers;
+                console.log(header.get('Content-Type'));
+                console.log(header.get('Bearer'));
+                console.log(header.get('Access-Control-Allow-Headers'));
+                console.log(header.get('Authorization'));
+                return header;
+            });
+
+       // console.log('object ' + object.headers.get('Authorization'));
+
+        const post = this.http
+            .post(this.authUrl, JSON.stringify({ username: username, password: password }), httpOptions)
+            .subscribe(
+            data => {
+               return data;
+            });
+        console.log('res ' + post.toString());
+
+        const a = this.http.post(this.authUrl, JSON.stringify({ username: username, password: password }), httpOptions);
         return this.http.post(this.authUrl, JSON.stringify({ username: username, password: password }), httpOptions)
             .map((response: Response) => {
                 // login successful if there's a jwt token in the response
+                const authHeader = response.headers.get('Authorization');
+                const authHeader1 = response.headers.get('Bearer');
+                console.log('Header :' + authHeader);
+                console.log(authHeader1);
                 const token = response.json() && response.json().token;
                 if (token) {
                     // store username and jwt token in local storage to keep user logged in between page refreshes
@@ -34,8 +80,14 @@ export class AuthenticationService {
                     // return false to indicate failed login
                     return false;
                 }
-            }).catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+            }).catch((error: any) => Observable.throw(error|| 'Server error'));
     }
+
+
+    private extractData(res: Response) {
+    let body = res.json();
+    return body || {};
+}
 
 
 }
